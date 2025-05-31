fdccdc4367232c9974c22b14e484b8b930e6b135
Cecy Geraldo <cecygeraldocodes@Cecy-MacBook-Pro.local>
2025-05-30 20:16:18 -0400

    feat: add new weather API to get complete and dynamic response

--- main.ts
+++ main.ts
@@ -1,36 +1,35 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
-
-// 1. Crear el servidor
-// Es la interfaze principal con el protocolo MCP. Maneja la comunicación entre el cliente y el servidor.
-
-const server = new McpServer({
-  name: "demo",
-  version: "1.0.0",
-});
-
-// 2. Definir la herramienta
-// Las herramientas la permite al LLM realizar acciones a través de tu servidor MCP.
-
-server.tool(
-  "fetch-weather",
-  "Tool to fetch the weather of a city",
-  {
-    city: z.string().describe("City name"),
-  },
-  async ({ city }) => {
+async ({ city }) => {
+    // 1. Geolocalización
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`);
+    const data = await response.json();
+  
+    if (!data.results || data.results.length === 0) {
+      return {
+        content: [
+          {
+            type: 'text',
+            text: `No se encontró información para la ciudad ${city}`
+          }
+        ]
+      };
+    }
+  
+    // 2. Extraer coordenadas
+    const { latitude, longitude } = data.results[0];
+  
+    // 3. Obtener clima
+    const weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,precipitation,is_day,rain&forecast_days=1`
+    );
+    const weatherData = await weatherResponse.json();
+  
+    // 4. Devolver resultado al LLM
     return {
       content: [
         {
-          type: "text",
-          text: `The weather of ${city} is sunny`,
-        },
-      ],
+          type: 'text',
+          text: JSON.stringify(weatherData, null, 2),
+        }
+      ]
     };
   }
-);
-
-//3. Escuchar las conexiones del cliente
-const transport = new StdioServerTransport();
-await server.connect(transport);
